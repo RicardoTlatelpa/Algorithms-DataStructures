a7739cb5eb923b7b08f8bdf7c9a9671256122f3c
Ricardo Tlatelpa <Ricardo.Tlatelpa42@myhunter.cuny.edu>
2022-07-15 20:42:28 -0400

    print tree perimeter analysis

--- educative/scripts/datastructures/binaryTrees/PrintTreePerimeter.py
+++ educative/scripts/datastructures/binaryTrees/PrintTreePerimeter.py
@@ -8,11 +8,11 @@ def display_tree_perimeter(root):
     
     result += str(root.data) + ", "
     current_node = root.left
-
+    # LEFT PERIMETER
     while current_node and current_node.left != None:
         result += str(current_node.data) + ", "
         current_node = current_node.left
-    
+    # BOTTOM PERIMETER
     stack = []
     current_node = root
     while current_node or stack:
@@ -27,6 +27,7 @@ def display_tree_perimeter(root):
 
         if p.right == None and p.left == None:
             result += str(p.data) + ", "
+    # RIGHT PERIMETER
     stack = []
     current_node = root.right
 
@@ -36,6 +37,6 @@ def display_tree_perimeter(root):
     while stack:
         p = stack.pop()
         result += str(p) + ", "
-
+    # Clean up result string to not include extra 2 characters: comma and space at the end
     result_ = result[:-2]
     print(str(result_), end="")
