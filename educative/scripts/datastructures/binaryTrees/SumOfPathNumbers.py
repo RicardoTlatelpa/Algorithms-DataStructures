37b21dc6a3876343d73bba0c3d114615c2ae52e8
Ricardo Tlatelpa <Ricardo.Tlatelpa42@myhunter.cuny.edu>
2022-07-17 23:39:42 -0400

    My solution analyzed

--- educative/scripts/datastructures/binaryTrees/SumOfPathNumbers.py
+++ educative/scripts/datastructures/binaryTrees/SumOfPathNumbers.py
@@ -10,25 +10,27 @@ class TreeNode:
     self.right = right
 
 def find_sum_of_path_numbers(root):
-  # TODO: Write your code here
-    if root is None:
-        return -1   
-    allPaths = []    
-    find_sum_of_path_numbers_helper(root, [], allPaths)
-    return allPaths
-    
-def find_sum_of_path_numbers_helper(root,currentPath, allPaths):
-    if root is None:
-        return
-    currentPath += str(root.val)
-    if root.left is None and root.right is None:           
-        integer = "".join(list(currentPath))
-        if allPaths:
-          allPaths.append(allPaths.pop() + int(integer))
-        else:
-          allPaths.append(int(integer))
+  if root is None:
+    return -1
+  
+  allPaths = []
+  find_sum_of_path_numbers_helper(root,[], allPaths)
+  return allPaths[0]
+
+def find_sum_of_path_numbers_helper(root,currentPath,allPaths):
+  if root is None:
+    return
+  currentPath += str(root.val)
+  if root.left is None and root.right is None:
+    integer = "".join(list(currentPath))
+    if allPaths:
+      # Perform addition by popping off the stack, leaving only one element in the stack
+      allPaths.append(allPaths.pop() + int(integer))
     else:
-        find_sum_of_path_numbers_helper(root.left, currentPath,allPaths)
-        find_sum_of_path_numbers_helper(root.right, currentPath,allPaths)
-    del currentPath[-1]  
-    
+      # We have reached the end of a path so we can append the integer of the string to allPaths
+      allPaths.append(int(integer))
+  else:
+    find_sum_of_path_numbers_helper(root.left, currentPath, allPaths)
+    find_sum_of_path_numbers_helper(root.right, currentPath, allPaths)
+  # backtracking case
+  del currentPath[-1]
