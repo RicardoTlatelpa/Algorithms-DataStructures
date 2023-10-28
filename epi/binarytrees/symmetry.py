881d9e70a4e47b17d3fd4ca1b842dc185522e9a3
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-10-28 13:32:57 -0400

    implemented symmetrical tree solution

--- epi/binarytrees/symmetry.py
+++ epi/binarytrees/symmetry.py
@@ -44,3 +44,24 @@ def is_symmetry(root):
         if l_val != r_val or l_dir == r_dir:
             return False
     return True
+
+'''
+Hint: the definition of symmetry is recursive
+
+Computing the mirror image of a tree is as simple
+as swapping the left and right subtrees, and recursively 
+continuing. The time and space complexity are both O(n), where
+n is the number of nodes in the tree.
+'''
+
+def is_symmetric(tree):
+    def check_symmetric(subtree_0, subtree_1):
+        if not subtree_0 and not subtree_1:
+            return True
+        elif subtree_0 and subtree_1:
+            return (subtree_0.val == subtree_1.val
+                    and check_symmetric(subtree_0.left, subtree_1.right)
+                    and check_symmetric(subtree_0.right, subtree_1.left)
+                    )
+        return False
+    return not tree or check_symmetric(tree.left,tree.right)
