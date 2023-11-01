c964c23469a247e9e73d8e2919ca5b4226587348
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-10-31 23:44:07 -0400

    solution for forming a linkedlist from leaaf nodes in a binary tree

--- epi/binarytrees/formllfromleaves.py
+++ epi/binarytrees/formllfromleaves.py
@@ -13,4 +13,40 @@ class TreeNode:
         self.right = right
 
 def create_ll(tree):
-    
+    dummy_node = TreeNode(None,None,None)
+    tail = dummy_node
+
+    def preorder(root):
+        nonlocal tail
+        if root is None:
+            return
+        if root and root.left == None and root.right == None:
+            tail.right = root
+            tail = tail.right
+        preorder(root.left)
+        preorder(root.right)
+
+    preorder(tree)
+    return dummy_node.right
+
+left_subtree = TreeNode(6,
+                    TreeNode(271,
+                        TreeNode(28,None,None),
+                        TreeNode(0,None,None)
+                             ),
+                    TreeNode(561,
+                            None,
+                             TreeNode(3,
+                                TreeNode(17,None,None),
+                                None
+                                      )
+                            )
+                )
+root = TreeNode(314,left_subtree,None)
+
+ll = create_ll(root)
+
+while(ll):
+    print(ll.val)
+    ll = ll.right
+
