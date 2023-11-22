87e67293d92e5c5340fe48437fb5911cea24c314
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-11-21 19:29:00 -0500

    adding solution  to 12-8

--- epi/hashTables/findLongestSubarray.py
+++ epi/hashTables/findLongestSubarray.py
@@ -41,3 +41,16 @@ def longest_subarray_with_distinct_entries(A):
     return ans
 A = ['f','s','f','e','t','w','e','n','w','e']
 print(longest_subarray_with_distinct_entries(A))
+
+def epi_longest_subarray_with_distinct_entries(A):
+    most_recent_occurrence = {}
+    longest_dup_free_subarray_start_idx = result = 0
+    for i,a in enumerate(A):
+        # defer updating dup_idx until we see a duplicate
+        if a in most_recent_occurrence:
+            dup_idx = most_recent_occurrence[a]
+            if dup_idx >= longest_dup_free_subarrray_start_idx:
+                result = max(result, i - longest_dup_free_subarray_start_idx)
+                longest_dup_free_subarray_start_idx = dup_idx + 1
+        most_recent_occurrence[a] = i
+    return max(result, len(A) - longest_dup_free_subarray_start_idx)
