c3067b281d1fe2087d51316b4acb39e50d042ef6
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-12-01 21:01:53 -0500

    computing alternation problem

---
+++ epi/arrays/computingAlternation.py
@@ -0,0 +1,26 @@
+'''
+5.8 Computing an alternation
+
+
+Write a program that takes an array A of n numbers, and 
+rearranges A's elements to get a new array B having 
+the property that B[0] < B[1] ≥ B[2] ≤ B[3] ≥ B[4] ≤ B[5] ≥ •*.
+'''
+
+# we could sort then rearrange
+# if everything is out of order
+    # it is difficult to know,
+    # if there exists something greater than
+    # the current number
+# there exists a number whose the last element
+
+def rearrange(A):
+    less_than = []
+    greater_than = []
+    for n in A:
+
+
+
+def rearrange_solution(A):
+
+
