1334d08baf3988f8441f540c4c86bdcd329ec03c
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-12-06 12:42:47 -0500

    added time complexity of function, slower than linear time and has a linear space

--- epi/arrays/5-9.py
+++ epi/arrays/5-9.py
@@ -14,3 +14,11 @@ def generate_primes(n):
     return primes
 
 print(generate_primes(1000))
+
+'''
+The bound we gave for the trial-division approach, namely O(n^3/2), is based on O(sqrt(n)) bound for
+each individual test. Since most numbers are not prime, the actual time complexity of trial-division
+is actually lower on average, since the test frequently early-returns false. It is known that the 
+time complexity of the trial-division approach is O(n^3/2/(logn)^2), so sieving is in fact superior
+to trial-divison.
+'''
