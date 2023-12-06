18c10fb4cfc97e46cd968af73ad144c092285a83
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-12-06 12:40:13 -0500

    solution to prime problem

---
+++ epi/arrays/5-9.py
@@ -0,0 +1,16 @@
+'''
+5.9 Generate primes from N
+'''
+
+
+def generate_primes(n):
+    primes = []
+    is_prime = [False,False]  + [True] * (n-1)
+    for p in range(2,n+1):
+        if is_prime[p]:
+            primes.append(p)
+            for i in range(p,n+1,p):
+                is_prime[i] = False
+    return primes
+
+print(generate_primes(1000))
