ba8bf2a3fd0b72f606eefb87cc9d1e01b847b288
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-12-06 12:32:05 -0500

    solved computing alternation problem

--- epi/arrays/computingAlternation.py
+++ epi/arrays/computingAlternation.py
@@ -7,20 +7,33 @@ rearranges A's elements to get a new array B having
 the property that B[0] < B[1] ≥ B[2] ≤ B[3] ≥ B[4] ≤ B[5] ≥ •*.
 '''
 
-# we could sort then rearrange
-# if everything is out of order
-    # it is difficult to know,
-    # if there exists something greater than
-    # the current number
-# there exists a number whose the last element
+'''
 
+Example:
+    [1,2,3] => [1,3,2]
+    [1]
+    [1,2]
+'''
 def rearrange(A):
-    less_than = []
-    greater_than = []
-    for n in A:
-
-
+    # we could rearrange the sorted array using 2 pointers
+    # left pointer pointing to the smallest number; 0
+    # right pointer pointing to the largest number; n-1
+    A.sort() # logN
+    n = len(A)-1 
+    left = 1
+    right = n
+    while(left < right):
+        A[left],A[right] = A[right],A[left]
+        left+=1
+        right-=1
+    return A
 
 def rearrange_solution(A):
+    A.sort()
+    for i in range(len(A)):
+        A[i:i+2] = sorted(A[i:i+2], reverse=i%2==1)
+    return A
+
+print(rearrange_solution([5,6,3,10,7,1,0]))
 
 
