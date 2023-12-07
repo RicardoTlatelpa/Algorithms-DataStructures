fec4cdd22fcfa8cb05671d7f6bcf25e549571d4d
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-12-07 17:00:20 -0500

    solution to 5.10 problem set

--- epi/arrays/5-10.py
+++ epi/arrays/5-10.py
@@ -5,5 +5,17 @@ Time complexity: N!
 Space complexity: stack space
 '''
 
-def apply_permutation()
+def apply_permutation(A,p):
+    permuted_array = [A[0]] * len(A)
+    ht = {}
+    for i in range(len(A)):
+        ht[i] = A[i]
+
+    for i in range(len(p)):
+        permutation = p[i]
+        permuted_array[permutation] = ht[i]
+
+    return permuted_array
+
+print(apply_permutation(['a','b','c','d'],[2,0,1,3]))
 
