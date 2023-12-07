53c3a6321b991725596707dc2eb6a2fd6c2c8b6c
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-12-07 17:06:07 -0500

    implemented EPI solution and wrote down its space and time complexity

--- epi/arrays/5-10.py
+++ epi/arrays/5-10.py
@@ -18,4 +18,28 @@ def apply_permutation(A,p):
     return permuted_array
 
 print(apply_permutation(['a','b','c','d'],[2,0,1,3]))
+'''
+Time Complexity: O(N)
+Space Complexity: O(N)
+'''
 
+def apply_permutation_solution(A,perm):
+    for i in range(len(A)):
+        next = i
+        while(perm[next] >= 0):
+            A[i], A[perm[next]] = A[perm[next]], A[i]
+            temp = perm[next]
+            perm[next] -= len(perm)
+            next = temp
+    perm[:] = [a + len(perm) for a in perm]
+    print(A)
+
+apply_permutation_solution(['a','b','c','d'], [2,0,1,3])
+
+'''
+The above solution improves the space complexity
+Time Complexity: O(N)
+Space Complexity: O(1)
+So the solution is much more efficient because of its space
+efficiency
+'''
