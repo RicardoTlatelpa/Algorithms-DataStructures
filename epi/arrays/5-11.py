40c59bab24d2885ec6544f10f090a6710f4dbe00
Ricardo Tlatelpa <rtlatelpa4@gmail.com>
2023-12-07 17:26:31 -0500

    next permutation solution, least optimal with N! time complexity and O(N) space complexity

---
+++ epi/arrays/5-11.py
@@ -0,0 +1,36 @@
+'''
+5.11 Compute the next permutation
+
+Solution, compute all the permutations (N! time)
+sort the permutations array and find the next permutation
+if the permutation is the last permutation of the sorted
+permutations array, pass the first element
+'''
+
+def next_permutation(perm):
+    all_permutations = [] # permutations as integers
+    visited = [False] * len(perm)
+    N = len(perm)
+    # i is the index
+    # visited is the visited array
+    # g is the generated string
+    def findPermutations(i,visited,g):
+        nonlocal all_permutations
+        if(len(g) == len(perm)):
+            # we found a permutation
+            all_permutations.append(int(g))
+            return
+        for idx in range(len(visited)):
+            if visited[idx] == False:
+                visited[idx] = True
+                findPermutations(idx,visited,g+perm[idx])
+                visited[idx] = False
+    findPermutations(0,visited,"")
+    all_permutations.sort()
+    curr_perm = int(''.join(perm))
+    for i in range(len(all_permutations)):
+        p = all_permutations[i]
+        if p ==curr_perm:
+            return_idx = (i+1)%len(all_permutations)
+            return all_permutations[return_idx]
+print(next_permutation(["1","2","3"]))
